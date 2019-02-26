178440a69639c2089733661a53d42e9d9235636e
kirakiray <pikay@foxmail.com>
2019-02-26 15:27:07 +0800

    version 3.1
    add frame module;

--- src/drill.js
+++ src/drill.js
@@ -7,6 +7,8 @@
     // 地址寄存器
     const bag = new Map();
 
+    window.bag = bag;
+
     // 映射资源
     const paths = new Map();
 
@@ -167,6 +169,117 @@
 
         // 设置完成
         packData.stat = 3;
+    });
+
+    // loaders添加iframe辅助线程支持
+    loaders.set("frame", async (packData) => {
+        // 新建iframe
+        let iframeEle = document.createElement("iframe");
+
+        // 设置不可见样式
+        Object.assign(iframeEle.style, {
+            position: "absolute",
+            border: "none",
+            outline: "none",
+            opacity: "1",
+            width: "100px",
+            height: "100px"
+        });
+
+        // 转换并获取真实链接
+        let {
+            link,
+            path
+        } = packData;
+
+        // 更新path
+        let newPath = path.replace(/\.frame$/, "/frame.html");
+
+        // 更新link
+        let newLink = link.replace(path, newPath);
+
+        // 设置链接
+        iframeEle.src = newLink;
+
+        // taskID记录器
+        let taskIDs = new Map();
+
+        // 添加计时器，当计算都完成时，计时10秒内，没有传入参数操作，就进行回收进程
+        let clearer = () => {
+            // 清除对象
+            bag.delete(path);
+
+            // 去除iframe
+            document.body.removeChild(iframeEle);
+
+            // 去除message监听
+            window.removeEventListener("message", messageFun);
+
+            // 快速内存回收
+            messageFun = packData = clearer = null;
+        };
+        packData.timer = setTimeout(clearer, 10000);
+
+        // 设置getPack函数
+        packData.getPack = (urlData) => new Promise(res => {
+            // 计算taskId
+            let taskId = getRandomId();
+
+            // 清除计时器
+            clearTimeout(packData.timer);
+
+            // 添加taskID和相应函数
+            taskIDs.set(taskId, {
+                res
+            });
+
+            // 发送数据过去
+            iframeEle.contentWindow.postMessage({
+                type: "drillFrameTask",
+                taskId,
+                data: urlData.data
+            }, '*');
+        })
+
+        // 在 windows上设置接收器
+        let messageFun;
+        window.addEventListener("message", messageFun = e => {
+            let {
+                data,
+                taskId
+            } = e.data;
+
+            // 判断是否在taskID内
+            if (taskIDs.has(taskId)) {
+                // 获取记录对象
+                let taskObj = taskIDs.get(taskId);
+
+                // 去除taskID
+                taskIDs.delete(taskId);
+
+                // 返回数据
+                taskObj.res(data);
+            }
+
+            // 当库存为0时，计时清理函数
+            if (!taskIDs.size) {
+                packData.timer = setTimeout(clearer, 10000);
+            }
+        });
+
+        // 加载完成函数
+        iframeEle.addEventListener('load', e => {
+            // 设置完成
+            packData.stat = 3;
+        });
+
+        // 错误函数
+        iframeEle.addEventListener('error', e => {
+            packData.stat = 2;
+        });
+
+        // 添加到body
+        document.body.appendChild(iframeEle);
     });
 
     // processors添加普通文件加载方式
