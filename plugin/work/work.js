92ce24d24dd91888d40634546626a6c8abc94824
huang yao <kirayao@hotmail.com>
2023-02-11 21:26:25 +0800

    docs: change the notes of the work module

--- plugin/work/work.js
+++ plugin/work/work.js
@@ -1,75 +1,75 @@
 if (typeof drill !== "undefined") {
-    // 在浏览器内
-    drill.ext(({ bag, addLoader, addProcess }) => {
-        addLoader("work", async ({ src, record }) => {
-            record.done((pkg) => {
-                return new Promise((resolve, reject) => {
-                    let worker = new Worker(src);
+  // In the browser, load the work plugin
+  drill.ext(({ bag, addLoader, addProcess }) => {
+    addLoader("work", async ({ src, record }) => {
+      record.done((pkg) => {
+        return new Promise((resolve, reject) => {
+          let worker = new Worker(src);
 
-                    worker.postMessage(pkg.data);
+          worker.postMessage(pkg.data);
 
-                    worker.onmessage = (e) => {
-                        if (e.data) {
-                            let { data, status } = e.data;
+          worker.onmessage = (e) => {
+            if (e.data) {
+              let { data, status } = e.data;
 
-                            if (status == "done") {
-                                resolve(data);
-                                worker.terminate();
-                            }
-                        }
-                    }
-                });
-            });
+              if (status == "done") {
+                resolve(data);
+                worker.terminate();
+              }
+            }
+          };
         });
+      });
     });
+  });
 } else {
-    // 在 worker 环境内
-    let data;
-    let getted_msg = false;
-    // 事件临时存储队列
-    const e_arr = [];
+  // Within the worker environment, add a shortcut to use the method
+  let data;
+  let getted_msg = false;
 
-    const run_work = async (func) => {
-        let reval = await func({
-            data
-        });
+  // Event Temporary Storage Queue
+  const e_arr = [];
 
-        postMessage({
-            status: "done",
-            data: reval
-        });
+  const run_work = async (func) => {
+    let reval = await func({
+      data,
+    });
 
-        // close();
+    postMessage({
+      status: "done",
+      data: reval,
+    });
+
+    // close();
+  };
+
+  onmessage = (e) => {
+    data = e.data;
+
+    if (e_arr.length) {
+      e_arr.forEach(run_work);
+      e_arr.length = 0;
+    }
+  };
+
+  let runned_work = false;
+
+  globalThis.work = async (callback) => {
+    if (runned_work) {
+      setTimeout(() => {
+        close();
+      }, 1000);
+      throw {
+        desc: "In the Worker, the work module can only be run once",
+      };
     }
 
-    onmessage = (e) => {
-        data = e.data;
-
-        if (e_arr.length) {
-            e_arr.forEach(run_work);
-            e_arr.length = 0;
-        }
+    // Run once
+    runned_work = true;
+    if (getted_msg) {
+      run_work(callback);
+    } else {
+      e_arr.push(callback);
     }
-
-    let runned_work = false;
-
-    // 虚拟work进程
-    globalThis.work = async (callback) => {
-        if (runned_work) {
-            setTimeout(() => {
-                close();
-            }, 1000);
-            throw {
-                desc: "In the Worker, the work module can only be run once"
-            };
-        }
-
-        // 运行一次
-        runned_work = true;
-        if (getted_msg) {
-            run_work(callback);
-        } else {
-            e_arr.push(callback);
-        }
-    };
+  };
 }
