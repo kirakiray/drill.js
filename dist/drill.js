015ef74d3b4b3bfff21f930ef8b4596b0d7f485a
huang yao <kirayao@hotmail.com>
2023-05-15 17:15:20 +0800

    refactor: change dist file name

--- dist/drill.js
+++ dist/drill.js
@@ -1,2 +1,160 @@
 // drill.js - v5.0.0 https://github.com/kirakiray/drill.js  (c) 2018-2023 YAO
-!function(){"use strict";const t={},e=(e,s)=>{if(e instanceof Function&&(s=e,e=["js","mjs"]),e instanceof Array)return void e.forEach((e=>{(t[e]||(t[e]=[])).push(s)}));(t[e]||(t[e]=[])).push(s)};e(["mjs","js"],(({url:t})=>import(t))),e(["txt","html"],(({url:t})=>fetch(t).then((t=>t.text())))),e("json",(async({url:t})=>fetch(t).then((t=>t.json())))),e("wasm",(async({url:t})=>{const e=await fetch(t).then((t=>t.arrayBuffer())),s=await WebAssembly.compile(e);return new WebAssembly.Instance(s).exports}));const s=async(e,s)=>{const n=new URL(e),{pathname:i}=n,r=i.slice(2+(i.lastIndexOf(".")-1>>>0));let c;const o=t[r];if(o)for(let t of o){const n=await t({url:e,data:c,...s});void 0!==n&&(c=n)}else c=fetch(e);return c};function n(t){return(t=>e=>{let n="";if(t.resolve)n=t.resolve(e);else{const s=new URL(t.url);n=new URL(e,s).href}return s(n)})(t)}Object.assign(n,{use:e});class i extends HTMLElement{constructor(...t){super(...t),this._init()}_init(){if(this.__initSrc||this.attributes.hasOwnProperty("pause"))return;let t=this.getAttribute("src");t&&(this.__initSrc=t,t=new URL(t,location.href,t).href,s(t,{element:this}))}attributeChangedCallback(t,e,s){"src"===t?s&&null===e?this._init():this.__initSrc&&e&&s!==this.__initSrc&&(console.warn(`${this.tagName.toLowerCase()} change src is invalid, only the first change will be loaded`),this.setAttribute("src",this.__initSrc)):"pause"===t&&null===s&&this._init()}static get observedAttributes(){return["src","pause"]}}customElements.define("load-module",i),customElements.define("l-m",class extends i{constructor(...t){super(...t)}}),"undefined"!=typeof window&&(window.lm=n),"object"==typeof module&&(module.exports=n)}();
+(function () {
+  'use strict';
+
+  const processor = {};
+
+  const use = (name, handler) => {
+    if (name instanceof Function) {
+      handler = name;
+      name = ["js", "mjs"];
+    }
+
+    if (name instanceof Array) {
+      name.forEach((name) => {
+        const tasks = processor[name] || (processor[name] = []);
+        tasks.push(handler);
+      });
+      return;
+    }
+
+    const tasks = processor[name] || (processor[name] = []);
+    tasks.push(handler);
+  };
+
+  use(["mjs", "js"], ({ url }) => {
+    return import(url);
+  });
+
+  use(["txt", "html"], ({ url }) => {
+    return fetch(url).then((e) => e.text());
+  });
+
+  use("json", async ({ url }) => {
+    return fetch(url).then((e) => e.json());
+  });
+
+  use("wasm", async ({ url }) => {
+    const data = await fetch(url).then((e) => e.arrayBuffer());
+
+    const module = await WebAssembly.compile(data);
+    const instance = new WebAssembly.Instance(module);
+
+    return instance.exports;
+  });
+
+  const createLoad = (meta) => {
+    const load = (url) => {
+      let reurl = "";
+      if (meta.resolve) {
+        reurl = meta.resolve(url);
+      } else {
+        const currentUrl = new URL(meta.url);
+        const resolvedUrl = new URL(url, currentUrl);
+        reurl = resolvedUrl.href;
+      }
+
+      return agent(reurl);
+    };
+    return load;
+  };
+
+  const agent = async (url, opts) => {
+    const urldata = new URL(url);
+    const { pathname } = urldata;
+
+    const type = pathname.slice(((pathname.lastIndexOf(".") - 1) >>> 0) + 2);
+
+    let data;
+
+    const tasks = processor[type];
+
+    if (tasks) {
+      for (let f of tasks) {
+        const temp = await f({
+          url,
+          data,
+          ...opts,
+        });
+
+        temp !== undefined && (data = temp);
+      }
+    } else {
+      data = fetch(url);
+    }
+
+    return data;
+  };
+
+  function lm(meta) {
+    return createLoad(meta);
+  }
+
+  Object.assign(lm, {
+    use,
+  });
+
+  class LoadModule extends HTMLElement {
+    constructor(...args) {
+      super(...args);
+
+      this._init();
+    }
+
+    _init() {
+      if (this.__initSrc || this.attributes.hasOwnProperty("pause")) {
+        return;
+      }
+
+      let src = this.getAttribute("src");
+
+      if (!src) {
+        return;
+        // throw `The ${this.tagName.toLowerCase()} element requires the src attribut `;
+      }
+
+      this.__initSrc = src;
+      src = new URL(src, location.href, src).href;
+      agent(src, {
+        element: this,
+      });
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === "src") {
+        if (newValue && oldValue === null) {
+          this._init();
+        } else if (this.__initSrc && oldValue && newValue !== this.__initSrc) {
+          console.warn(
+            `${this.tagName.toLowerCase()} change src is invalid, only the first change will be loaded`
+          );
+          this.setAttribute("src", this.__initSrc);
+        }
+      } else if (name === "pause" && newValue === null) {
+        this._init();
+      }
+    }
+
+    static get observedAttributes() {
+      return ["src", "pause"];
+    }
+  }
+
+  class LM extends LoadModule {
+    constructor(...args) {
+      super(...args);
+    }
+  }
+
+  customElements.define("load-module", LoadModule);
+  customElements.define("l-m", LM);
+
+  if (typeof window !== "undefined") {
+    window.lm = lm;
+  }
+
+  if (typeof module === "object") {
+    module.exports = lm;
+  }
+
+})();
