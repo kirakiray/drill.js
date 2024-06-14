392d36e34246ebd3c522af2c4aaad20da0a7ab29
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-06-14 17:13:23 +0800

    Update test-server-render.js

--- scripts/test-server-render.js
+++ scripts/test-server-render.js
@@ -3,7 +3,7 @@ module.exports = async (app) => {
   app.use(async (ctx) => {
     const { originalUrl } = ctx;
 
-    if (originalUrl === "/__test_js") {
+    if (originalUrl === "/_test_js") {
       ctx.set("Content-Type", "application/javascript; charset=utf-8");
       ctx.body = `
       export const val = 'I am _test_js';
