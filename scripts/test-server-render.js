d08acd9b72128faf50415a722d49f8f63c25a1a0
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-06-14 16:52:06 +0800

    feat: Support some content-type formats

---
+++ scripts/test-server-render.js
@@ -0,0 +1,13 @@
+module.exports = async (app) => {
+  // test server render
+  app.use(async (ctx) => {
+    const { originalUrl } = ctx;
+
+    if (originalUrl === "/__test_js") {
+      ctx.set("Content-Type", "application/javascript; charset=utf-8");
+      ctx.body = `
+      export const val = 'I am _test_js';
+      `;
+    }
+  });
+};
