ffaf19010e6a1faecdd6a25ffdd5af8e1d05d1c8
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-04 17:26:13 +0800

    Update main.spec.js

--- test/main.spec.js
+++ test/main.spec.js
@@ -3,10 +3,6 @@ const { test, expect } = require("@playwright/test");
 test("load module are all correct", async ({ page }) => {
   await page.goto("http://localhost:3340/test/statics/load-module.html");
 
-  setTimeout(() => {
-    page;
-  }, 2000);
-
   await page.getByRole("link", { name: "• load in ctx mode" }).click();
   await page.goBack();
   await page.getByRole("link", { name: "• load css by element" }).click();
@@ -21,6 +17,8 @@ test("load module are all correct", async ({ page }) => {
   await page.goBack();
   await page.getByRole("link", { name: "• load es module succeed" }).click();
   await page.goBack();
+
+  await expect(true).toBe(true);
 });
 
 test("use", async ({ page }) => {
