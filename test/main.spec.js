182009509e02dc74549f21e0bf63ff22ad42037b
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-04 16:22:16 +0800

    Update main.spec.js

--- test/main.spec.js
+++ test/main.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require("@playwright/test");
 test("load module are all correct", async ({ page }) => {
   await page.goto("http://localhost:3340/test/statics/load-module.html");
 
-  await new Promise((res) => setTimeout(res, 1200));
+  await new Promise((res) => setTimeout(res, 2000));
 
   await expect((await page.$$(".jasmine-specs .jasmine-passed")).length).toBe(
     7
