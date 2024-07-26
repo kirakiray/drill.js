cf0427745b3baf85db2f7d1c79b7ba8b9f168598
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-26 17:26:26 +0800

    fix: little issuse

--- dist/drill.mjs
+++ dist/drill.mjs
@@ -1,4 +1,4 @@
-//! drill.js - v5.3.10 https://github.com/kirakiray/drill.js  (c) 2018-2024 YAO
+//! drill.js - v5.3.11 https://github.com/kirakiray/drill.js  (c) 2018-2024 YAO
 // const error_origin = "http://127.0.0.1:5793/errors";
 const error_origin = "https://ofajs.github.io/ofa-errors/errors";
 
